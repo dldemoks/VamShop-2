cc3ae45f412f3080ce3603e254c838f855eb63be
dldemoks <demokstar@gmail.com>
2016-03-16 18:01:31 +0700

    update

--- app/Plugin/Payment/Controller/PayeerController.php
+++ app/Plugin/Payment/Controller/PayeerController.php
@@ -23,7 +23,7 @@ class PayeerController extends PaymentAppController
 
 		$new_module['PaymentMethodValue'][0]['payment_method_id'] = $this->PaymentMethod->id;
 		$new_module['PaymentMethodValue'][0]['key'] = 'm_url';
-		$new_module['PaymentMethodValue'][0]['value'] = '//payeer.com/merchant/';
+		$new_module['PaymentMethodValue'][0]['value'] = 'https://payeer.com/merchant/';
 
 		$new_module['PaymentMethodValue'][1]['payment_method_id'] = $this->PaymentMethod->id;
 		$new_module['PaymentMethodValue'][1]['key'] = 'm_shop';
@@ -34,23 +34,18 @@ class PayeerController extends PaymentAppController
 		$new_module['PaymentMethodValue'][2]['value'] = '';
 		
 		$new_module['PaymentMethodValue'][3]['payment_method_id'] = $this->PaymentMethod->id;
-		$new_module['PaymentMethodValue'][3]['key'] = 'm_desc';
+		$new_module['PaymentMethodValue'][3]['key'] = 'pathlog';
 		$new_module['PaymentMethodValue'][3]['value'] = '';
 		
 		$new_module['PaymentMethodValue'][4]['payment_method_id'] = $this->PaymentMethod->id;
-		$new_module['PaymentMethodValue'][4]['key'] = 'pathlog';
+		$new_module['PaymentMethodValue'][4]['key'] = 'ipfilter';
 		$new_module['PaymentMethodValue'][4]['value'] = '';
 		
 		$new_module['PaymentMethodValue'][5]['payment_method_id'] = $this->PaymentMethod->id;
-		$new_module['PaymentMethodValue'][5]['key'] = 'ipfilter';
+		$new_module['PaymentMethodValue'][5]['key'] = 'emailerr';
 		$new_module['PaymentMethodValue'][5]['value'] = '';
-		
-		$new_module['PaymentMethodValue'][6]['payment_method_id'] = $this->PaymentMethod->id;
-		$new_module['PaymentMethodValue'][6]['key'] = 'emailerr';
-		$new_module['PaymentMethodValue'][6]['value'] = '';
 
 		$this->PaymentMethod->saveAll($new_module);
-
 		$this->Session->setFlash(__('Module Installed'));
 		$this->redirect('/payment_methods/admin/');
 	}
@@ -67,28 +62,34 @@ class PayeerController extends PaymentAppController
 	{
 		$order = $this->Order->read(null, $_SESSION['Customer']['order_id']);
 		
-		$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
-
-		$payeer_url = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'm_url')));
+		$payment_method = $this->PaymentMethod->find('first', array(
+			'conditions' => array(
+				'alias' => $this->module_name
+			)
+		));
+		$payeer_url = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+			'conditions' => array(
+				'key' => 'm_url'
+			)
+		));
+		$payeer_shop = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+			'conditions' => array(
+				'key' => 'm_shop'
+			)
+		));
+		$payeer_key = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+			'conditions' => array(
+				'key' => 'm_key'
+			)
+		));
+		
 		$m_url = $payeer_url['PaymentMethodValue']['value'];
-		
-		$payeer_shop = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'm_shop')));
 		$m_shop = $payeer_shop['PaymentMethodValue']['value'];
-		
-		$payeer_key = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'm_key')));
 		$m_key = $payeer_key['PaymentMethodValue']['value'];
-		
-		$payeer_desc = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'm_desc')));
-		$m_desc = base64_encode($payeer_desc['PaymentMethodValue']['value']);
-		
+		$m_desc = base64_encode($order['OrderComment'][0]['comment']);
 		$m_orderid = $_SESSION['Customer']['order_id'];
 		$m_amount = number_format($order['Order']['total'], 2, '.', '');
-		$m_curr = $_SESSION['Customer']['currency_code'];
-		
-		if ($m_curr == 'RUR')
-		{
-			$m_curr = 'RUB';
-		}
+		$m_curr = $_SESSION['Customer']['currency_code'] == 'RUR' ? 'RUB' : $_SESSION['Customer']['currency_code'];
 
 		$arHash = array(
 			$m_shop,
@@ -98,6 +99,7 @@ class PayeerController extends PaymentAppController
 			$m_desc,
 			$m_key
 		);
+
 		$sign = strtoupper(hash('sha256', implode(':', $arHash)));
 
 		$content = '
@@ -122,37 +124,80 @@ class PayeerController extends PaymentAppController
 			$order['Order'][$key] = $value;
 		}
 		
-		$default_status = $this->Order->OrderStatus->find('first', array('conditions' => array('default' => '1')));
-		$order['Order']['order_status_id'] = $default_status['OrderStatus']['id'];
+		$default_status = $this->Order->OrderStatus->find('first', array(
+			'conditions' => array(
+				'default' => '1'
+			)
+		));
 		
+		$order['Order']['order_status_id'] = $default_status['OrderStatus']['id'];
 		$this->Order->save($order);
 
 		return $content;
 	}
 
-	public function after_process()
-	{
-	}
+	public function after_process(){}
 	
 	public function result()
 	{
-		$this->layout = false;
-		
 		if (isset($_POST["m_operation_id"]) && isset($_POST["m_sign"]))
 		{
-			$payeer_key = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'm_key')));
-			$m_key = $payeer_key['PaymentMethodValue']['value'];
+			$err = false;
+			$message = '';
+			$this->layout = false;
 			
-			$payeer_ipfilter = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'ipfilter')));
-			$ipfilter = $payeer_ipfilter['PaymentMethodValue']['value'];
+			$payeer_pathlog = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+				'conditions' => array(
+					'key' => 'pathlog'
+				)
+			));
+			$payeer_key = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+				'conditions' => array(
+					'key' => 'm_key'
+				)
+			));
+			$payeer_ipfilter = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+				'conditions' => array(
+					'key' => 'ipfilter'
+				)
+			));
+			$payeer_emailerr = $this->PaymentMethod->PaymentMethodValue->find('first', array(
+				'conditions' => array(
+					'key' => 'emailerr'
+				)
+			));
 			
-			$payeer_pathlog = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'pathlog')));
 			$pathlog = $payeer_pathlog['PaymentMethodValue']['value'];
-			
-			$payeer_emailerr = $this->PaymentMethod->PaymentMethodValue->find('first', array('conditions' => array('key' => 'emailerr')));
+			$m_key = $payeer_key['PaymentMethodValue']['value'];
+			$ipfilter = $payeer_ipfilter['PaymentMethodValue']['value'];
 			$emailerr = $payeer_emailerr['PaymentMethodValue']['value'];
 			
-			$arHash = array(
+			// запись логов
+			
+			$log_text = 
+			"--------------------------------------------------------\n" .
+			"operation id		" . $_POST['m_operation_id'] . "\n" .
+			"operation ps		" . $_POST['m_operation_ps'] . "\n" .
+			"operation date		" . $_POST['m_operation_date'] . "\n" .
+			"operation pay date	" . $_POST['m_operation_pay_date'] . "\n" .
+			"shop				" . $_POST['m_shop'] . "\n" .
+			"order id			" . $_POST['m_orderid'] . "\n" .
+			"amount				" . $_POST['m_amount'] . "\n" .
+			"currency			" . $_POST['m_curr'] . "\n" .
+			"description		" . base64_decode($_POST['m_desc']) . "\n" .
+			"status				" . $_POST['m_status'] . "\n" .
+			"sign				" . $_POST['m_sign'] . "\n\n";
+			
+			$log_file = $pathlog;
+			
+			if (!empty($log_file))
+			{
+				file_put_contents($_SERVER['DOCUMENT_ROOT'] . $log_file, $log_text, FILE_APPEND);
+			}
+			
+			// проверка цифровой подписи и ip
+
+			$sign_hash = strtoupper(hash('sha256', implode(":", array(
 				$_POST['m_operation_id'],
 				$_POST['m_operation_ps'],
 				$_POST['m_operation_date'],
@@ -163,98 +208,110 @@ class PayeerController extends PaymentAppController
 				$_POST['m_curr'],
 				$_POST['m_desc'],
 				$_POST['m_status'],
-				$m_key);
-			$sign_hash = strtoupper(hash('sha256', implode(":", $arHash)));
+				$m_key
+			))));
 			
-			// проверка принадлежности ip списку доверенных ip
-			$list_ip_str = str_replace(' ', '', $ipfilter);
+			$valid_ip = true;
+			$sIP = str_replace(' ', '', $ipfilter);
 			
-			if (!empty($list_ip_str)) 
+			if (!empty($sIP))
 			{
-				$list_ip = explode(',', $list_ip_str);
-				$this_ip = $_SERVER['REMOTE_ADDR'];
-				$this_ip_field = explode('.', $this_ip);
-				$list_ip_field = array();
-				$i = 0;
-				$valid_ip = FALSE;
-				foreach ($list_ip as $ip)
+				$arrIP = explode('.', $_SERVER['REMOTE_ADDR']);
+				if (!preg_match('/(^|,)(' . $arrIP[0] . '|\*{1})(\.)' .
+				'(' . $arrIP[1] . '|\*{1})(\.)' .
+				'(' . $arrIP[2] . '|\*{1})(\.)' .
+				'(' . $arrIP[3] . '|\*{1})($|,)/', $sIP))
 				{
-					$ip_field[$i] = explode('.', $ip);
-					if ((($this_ip_field[0] ==  $ip_field[$i][0]) or ($ip_field[$i][0] == '*')) and
-						(($this_ip_field[1] ==  $ip_field[$i][1]) or ($ip_field[$i][1] == '*')) and
-						(($this_ip_field[2] ==  $ip_field[$i][2]) or ($ip_field[$i][2] == '*')) and
-						(($this_ip_field[3] ==  $ip_field[$i][3]) or ($ip_field[$i][3] == '*')))
-						{
-							$valid_ip = TRUE;
-							break;
-						}
-					$i++;
+					$valid_ip = false;
 				}
 			}
-			else
+			
+			if (!$valid_ip)
 			{
-				$valid_ip = TRUE;
+				$message .= __d('payeer',' - the ip address of the server is not trusted') . "\n" .
+				__d('payeer','   trusted ip: ') . $ipfilter . "\n" .
+				__d('payeer','   ip of the current server: ') . $_SERVER['REMOTE_ADDR'] . "\n";
+				$err = true;
+			}
+
+			if ($_POST['m_sign'] != $sign_hash)
+			{
+				$message .= __d('payeer',' - Do not match the digital signature') . "\n";
+				$err = true;
 			}
 			
-			$log_text = 
-				"--------------------------------------------------------\n".
-				"operation id		".$_POST["m_operation_id"]."\n".
-				"operation ps		".$_POST["m_operation_ps"]."\n".
-				"operation date		".$_POST["m_operation_date"]."\n".
-				"operation pay date	".$_POST["m_operation_pay_date"]."\n".
-				"shop				".$_POST["m_shop"]."\n".
-				"order id			".$_POST["m_orderid"]."\n".
-				"amount				".$_POST["m_amount"]."\n".
-				"currency			".$_POST["m_curr"]."\n".
-				"description		".base64_decode($_POST["m_desc"])."\n".
-				"status				".$_POST["m_status"]."\n".
-				"sign				".$_POST["m_sign"]."\n\n";
-					
-			if (!empty($pathlog))
-			{	
-				file_put_contents($_SERVER['DOCUMENT_ROOT'] . $pathlog, $log_text, FILE_APPEND);
-			}
-			
-			if ($_POST["m_sign"] == $sign_hash && $_POST['m_status'] == "success" && $valid_ip)
+			if (!$err)
 			{
-				$payment_method = $this->PaymentMethod->find('first', array('conditions' => array('alias' => $this->module_name)));
-				$order_data = $this->Order->find('first', array('conditions' => array('Order.id' => $_POST['m_orderid'])));
-				$order_data['Order']['order_status_id'] = $payment_method['PaymentMethod']['order_status_id'];
-				$this->Order->save($order_data);
+				// загрузка заказа
+
+				$order = $this->Order->read(null, $_POST['m_orderid']);
 				
-				exit($_POST['m_orderid'] . '|success');
+				if (!$order)
+				{
+					$message .= __d('payeer',' - undefined order id') . "\n";
+					$err = true;
+				}
+				else
+				{
+					$order_amount = number_format($order['Order']['total'], 2, '.', '');
+					
+					// проверка суммы
+				
+					if ($_POST['m_amount'] != $order_amount)
+					{
+						$message .= __d('payeer',' - wrong amount') . "\n";
+						$err = true;
+					}
+					
+					// проверка статуса
+					
+					if (!$err)
+					{
+						switch ($_POST['m_status'])
+						{
+							case 'success':
+								$payment_method = $this->PaymentMethod->find('first', array(
+									'conditions' => array(
+										'alias' => $this->module_name
+									)
+								));
+								$order_data = $this->Order->find('first', array(
+									'conditions' => array(
+										'Order.id' => $_POST['m_orderid']
+									)
+								));
+								$order_data['Order']['order_status_id'] = $payment_method['PaymentMethod']['order_status_id'];
+								$this->Order->save($order_data);
+								break;
+								
+							default:
+								$message .= __d('payeer',' - The payment status is not success') . "\n";
+								$err = true;
+								break;
+						}
+					}
+				}
 			}
-			else
+			
+			if ($err)
 			{
 				$to = $emailerr;
-				$subject = __d('payeer','Error payment');
-				$message = __d('payeer','Failed to make the payment through the system Payeer for the following reasons') . ":\n\n";
-				if ($_POST["m_sign"] != $sign_hash)
+
+				if (!empty($to))
 				{
-					$message .= __d('payeer',' - Do not match the digital signature') . "\n";
+					$message = __d('payeer','Failed to make the payment through the system Payeer for the following reasons') . ":\n\n" . $message . "\n" . $log_text;
+					$headers = "From: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" . 
+					"Content-type: text/plain; charset=utf-8 \r\n";
+					mail($to, __d('payeer','Error payment'), $message, $headers);
 				}
-				if ($_POST['m_status'] != "success")
-				{
-					$message .= __d('payeer',' - The payment status is not success') . "\n";
-				}
-				if (!$valid_ip)
-				{
-					$message .= __d('payeer',' - the ip address of the server is not trusted') . "\n";
-					$message .= __d('payeer','   trusted ip: ') . $ipfilter . "\n";
-					$message .= __d('payeer','   ip of the current server: ') . $_SERVER['REMOTE_ADDR'] . "\n";
-				}
-				$message .= "\n" . $log_text;
-				$headers = "From: no-reply@" . $_SERVER['HTTP_SERVER'] . "\r\nContent-type: text/plain; charset=utf-8 \r\n";
-				mail($to, $subject, $message, $headers);
-				exit ($_POST['m_orderid'] . '|error');
+				
+				exit($_POST['m_orderid'] . '|error');
+			}
+			else
+			{
+				exit($_POST['m_orderid'] . '|success');
 			}
 		}
-			
-			
-		
-		
 	}
-	
 }
-
 ?>
--- app/webroot/payeer_fail.php
+++ app/webroot/payeer_fail.php
@@ -1,78 +1,77 @@
-<?
-header("Content-Type: text/html; charset=utf-8");
-
-$mes = '<p>Ваш заказ № ' . intval($_GET['m_orderid']) . ' оплачен неуспешно.</p>';
-
+<?php
+	header("Content-Type: text/html; charset=utf-8");
+	$order_id = preg_replace('/[^a-zA-Z0-9_-]/', '', substr($_GET['m_orderid'], 0, 32));
+	$mes = '<p>Ваш заказ № ' . $order_id . ' оплачен неуспешно.</p>';
 ?>
 
 <html>
 	<link rel="stylesheet" type="text/css" href="css/vamshop.css"/>
 	<head>
 		<style>
-			body
-			{
-				font-size: 25px;
-				background-color: #395969; 
+		body
+		{
+			font-size: 25px;
+			background-color: #395969; 
+		}
+		
+		.btn
+		{
+			cursor: pointer;
+		}
+		.btn:hover
+		{
+			color: #fff;
+		}
+		
+		.payment_message
+		{
+			text-align: center;
+			font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+			padding-top: 40px;
+			box-shadow: 0px 0px 10px #fff;
+			background-color: #ffffff;
+			position: fixed;
+			width: 500px;
+			height: 200px;
+			left: 50%;
+			top: 30%;
+			margin-left: -250px;
+			margin-top: -100px;
+			z-index: 1005;
+			animation-name: payment_message;
+			-webkit-animation-name: payment_message;	
+			animation-duration: 0.5s;	
+			-webkit-animation-duration: 0.5s;
+			animation-timing-function: ease-out;	
+			-webkit-animation-timing-function: ease-out;
+		}
+		@keyframes payment_message {
+			0% {
+				transform: scale(1) rotate(0deg) translateX(0%) translateY(-10%);
+				opacity: 0;
 			}
-			
-			.btn
-			{
-				cursor: pointer;
+			100% {
+				transform: scale(1) rotate(0deg) translateX(0%) translateY(0%);
+				opacity: 1;
+			}		
+		}
+		@-webkit-keyframes payment_message {
+			0% {
+				-webkit-transform: scale(1) rotate(0deg) translateX(0%) translateY(-10%);
+				opacity: 0;
 			}
-			.btn:hover
-			{
-				color: #fff;
-			}
-			
-			.payment_message
-			{
-				text-align: center;
-				font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
-				padding-top: 40px;
-				box-shadow: 0px 0px 10px #fff;
-				background-color: #ffffff;
-				position: fixed;
-				width: 500px;
-				height: 200px;
-				left: 50%;
-				top: 30%;
-				margin-left: -250px;
-				margin-top: -100px;
-				z-index: 1005;
-				animation-name: payment_message;
-				-webkit-animation-name: payment_message;	
-				animation-duration: 0.5s;	
-				-webkit-animation-duration: 0.5s;
-				animation-timing-function: ease-out;	
-				-webkit-animation-timing-function: ease-out;
-			}
-			@keyframes payment_message {
-				0% {
-					transform: scale(1) rotate(0deg) translateX(0%) translateY(-10%);
-					opacity: 0;
-				}
-				100% {
-					transform: scale(1) rotate(0deg) translateX(0%) translateY(0%);
-					opacity: 1;
-				}		
-			}
-			@-webkit-keyframes payment_message {
-				0% {
-					-webkit-transform: scale(1) rotate(0deg) translateX(0%) translateY(-10%);
-					opacity: 0;
-				}
-				100% {
-					-webkit-transform: scale(1) rotate(0deg) translateX(0%) translateY(0%);
-					opacity: 1;
-				}				
-			}
+			100% {
+				-webkit-transform: scale(1) rotate(0deg) translateX(0%) translateY(0%);
+				opacity: 1;
+			}				
+		}
 		</style>
 		<title>Заказ не оплачен</title>
 	</head>
 	
 	<body>
 		<div class='payment_message'>
-			<h4><?=$mes;?></h4>
+			<h4><?php echo $mes; ?></h4>
 			<button class="btn btn-inverse" type="submit" onCLick="location='http://<?php echo $_SERVER['HTTP_HOST'];?>';"> 
 				Вернуться в магазин
 			</button>
